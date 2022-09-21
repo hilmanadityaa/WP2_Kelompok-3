9ae02551058ea32ee21267213c6015d103704032
Hilman Aditya <12210686@bsi.ac.id>
2022-09-21 21:40:47 +0700

    commit pertama

---
+++ Hilman/application/views/views-latihan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Latihan 1</title>
+</head>
+<body>
+	hallo kawan.. yuk kita belajar web programming..!!! <br>
+	nilai 1 = <?= $nilai1; ?>
+	nilai 2 = <?= $nilai2; ?>
+	ini hasil dari permodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
